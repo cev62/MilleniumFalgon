3e183fa16dbe2c8b8a074212a3b72b50ec8b305b
Aaron Maharry <aaronmaharry@gmail.com>
2014-05-29 16:31:52 -0400

    Update

--- MilleniumFalgon.ino
+++ MilleniumFalgon.ino
@@ -2,22 +2,36 @@
 
 int left_motor_forward = 13;
 int left_motor_reverse = 11;
-int left_motor_direction_forward = 8;
-int left_motor_direction_reverse = 7;
+int left_motor_direction_forward = 7;
+int left_motor_direction_reverse = 8;
 
 int right_motor_forward = 6;
 int right_motor_reverse = 5;
-int right_motor_direction_forward = 3;
-int right_motor_direction_reverse = 2;
+int right_motor_direction_forward = 2;
+int right_motor_direction_reverse = 3;
+
+float deadzone = 0.1;
 
 int rx = 9;
 int tx = 10;
 SoftwareSerial blueSerial(rx, tx);
 
-float left_percent = 0.0;
-float right_percent = 0.0;
+float left_percent;
+float right_percent;
 
-void SetDriveLR(float left_percent, float right_percent);
+void SetMotor(int pwm_forward, int pwn_reverse, int direction_forward, int direction_reverse, float percent_power);
+void SetDriveLR();
+
+typedef enum ControlState
+{
+  NONE = 0,
+  BLUETOOTH,
+  WIRED
+} ControlState;
+
+ControlState state;
+int watchdog_timeout = 1000;
+unsigned long watchdog_timer_start;
 
 void setup()
 {
@@ -29,98 +43,115 @@ void setup()
   pinMode(right_motor_reverse, OUTPUT);
   pinMode(right_motor_direction_forward, OUTPUT);
   pinMode(right_motor_direction_reverse, OUTPUT);
+  
+  left_percent = 0.0;
+  right_percent = 0.0;
+  SetDriveLR();
+  
   blueSerial.begin(9600);
   Serial.begin(9600);
-  SetDriveLR(0.0, 0.0);
+  
+  state = NONE;
+  watchdog_timer_start = millis();
 }
 
 void loop()
 {
-  while(blueSerial.available() > 0)
+  if(millis() - watchdog_timer_start > watchdog_timeout && (state == BLUETOOTH || state == WIRED))
+  {
+    // Watchdog timed out: revert to control state NONE
+    state = NONE;
+    Serial.println("Watchdog timeout...");
+    blueSerial.println("Watchdog timeout...");
+  }  
+  
+  bool is_bluetooth_received = blueSerial.available();
+  if(is_bluetooth_received)
   {
     int left = blueSerial.parseInt();
     int right = blueSerial.parseInt();
-    
     if(blueSerial.read() == ';')
     {
-      blueSerial.print("Received");
-      SetDriveLR((float)left / 255, (float)right / 255);
+      left_percent = (float)left / 255;
+      right_percent = (float)right / 255;
+      state = BLUETOOTH;
+      blueSerial.println("State: BLUETOOTH (received)");
+      Serial.println("State: BLUETOOTH (received)");
+      watchdog_timer_start = millis();
+    }
+  }  
+  
+  bool is_wired_received = Serial.available();
+  if(is_wired_received)
+  {
+    int left = Serial.parseInt();
+    int right = Serial.parseInt();
+    if(Serial.read() == ';')
+    {
+      left_percent = (float)left / 255;
+      right_percent = (float)right / 255;
+      state = WIRED;
+      blueSerial.println("State: WIRED (received)");
+      Serial.println("State: WIRED (received)");
+      watchdog_timer_start = millis();
+    }
+  }
+  
+  if(state == NONE)
+  {
+    left_percent = 0.0;
+    right_percent = 0.0;
+    SetDriveLR();
+  }
+  else if (state == BLUETOOTH)
+  {
+    SetDriveLR();
+  }
+  else if (state == WIRED)
+  {
+    SetDriveLR();
+  }
+  
+}
+
+void SetMotor(int pwm_forward, int pwm_reverse, int direction_forward, int direction_reverse, float percent_power)
+{
+  bool reverse = false;
+  float power = min(1.0, max(-1.0, percent_power));
+  if(power < 0)
+  {
+    power = -power;
+    reverse = true;
+  }
+  
+  if(power < deadzone && power > -deadzone)
+  {
+    digitalWrite(direction_reverse, LOW);
+    digitalWrite(direction_forward, LOW);
+    analogWrite(pwm_reverse, 0);
+    analogWrite(pwm_forward, 0);
+  }
+  else
+  {
+    if(!reverse)
+    {
+      digitalWrite(direction_reverse, LOW);
+      digitalWrite(direction_forward, HIGH);
+      analogWrite(pwm_reverse, 0);
+      analogWrite(pwm_forward, (int)(255 * power));
+    }
+    else
+    {
+      digitalWrite(direction_reverse, HIGH);
+      digitalWrite(direction_forward, LOW);
+      analogWrite(pwm_reverse, (int)(255 * power));
+      analogWrite(pwm_forward, 0);
     }
   }
 }
 
-void SetDriveLR(float left_percent_in, float right_percent_in)
+void SetDriveLR()
 {
-  float left_percent = left_percent_in, right_percent = right_percent_in;
-  boolean left_reverse = false, right_reverse = false;
-  
-  // arguments are on [-1.0, 1.0] range
-  left_percent = min(1.0, max(-1.0, left_percent));
-  right_percent = min(1.0, max(-1.0, right_percent));
-  if(left_percent < 0)
-  {
-    left_percent = -left_percent;
-    left_reverse = true;
-  }
-  if(right_percent < 0)
-  {
-    right_percent = -right_percent;
-    right_reverse = true;
-  }
-  
-  if(left_percent == 0.0)
-  {
-    digitalWrite(left_motor_direction_reverse, LOW);
-    digitalWrite(left_motor_direction_forward, LOW);
-    analogWrite(left_motor_reverse, (int)(0));
-    analogWrite(left_motor_forward, (int)(0));
-  }
-  else
-  {
-    // Actuate!
-    if(!left_reverse)
-    {
-      digitalWrite(left_motor_direction_reverse, HIGH);
-      digitalWrite(left_motor_direction_forward, LOW);
-      analogWrite(left_motor_reverse, (int)(0));
-      analogWrite(left_motor_forward, (int)(255 * left_percent));
-    }
-    else
-    {
-      digitalWrite(left_motor_direction_reverse, LOW);
-      digitalWrite(left_motor_direction_forward, HIGH);
-      analogWrite(left_motor_reverse, (int)(255 * left_percent));
-      analogWrite(left_motor_forward, (int)(0));
-    }
-  }
-  
-  if(right_percent == 0.0)
-  {
-    digitalWrite(right_motor_direction_reverse, LOW);
-    digitalWrite(right_motor_direction_forward, LOW);
-    analogWrite(right_motor_reverse, (int)(0));
-    analogWrite(right_motor_forward, (int)(0));
-  }
-  else
-  {
-    if(!right_reverse)
-    {
-      digitalWrite(right_motor_direction_reverse, HIGH);
-      digitalWrite(right_motor_direction_forward, LOW);
-      analogWrite(right_motor_reverse, (int)(0));
-      analogWrite(right_motor_forward, (int)(255 * right_percent));
-    }
-    else
-    {
-      digitalWrite(right_motor_direction_reverse, LOW);
-      digitalWrite(right_motor_direction_forward, HIGH);
-      analogWrite(right_motor_reverse, (int)(255 * right_percent));
-      analogWrite(right_motor_forward, (int)(0));
-    }
-  }
-  
-  Serial.write((int)(10 * left_percent_in) + 48);
-  Serial.write((int)(10 * right_percent_in) + 48);
-  blueSerial.write((int)(10 * left_percent_in) + 48);
-  blueSerial.write((int)(10 * right_percent_in) + 48);
+  SetMotor(left_motor_forward, left_motor_reverse, left_motor_direction_forward, left_motor_direction_reverse, left_percent);
+  SetMotor(right_motor_forward, right_motor_reverse, right_motor_direction_forward, right_motor_direction_reverse, right_percent);
 }
